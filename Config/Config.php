df7cf3b41358f435f3a058906c47d5780c5d054a
WalterBen2015 <145167716+WalterBen2015@users.noreply.github.com>
2024-06-03 21:55:06 -0300

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/biblioteca-Epes68/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "BaseDato";
+const charset = "charset=utf8";
+?>
